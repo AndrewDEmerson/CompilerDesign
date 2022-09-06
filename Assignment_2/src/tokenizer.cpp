d0256b2dd2f169e4c9c98bcf3a57915a827498c9
vincent-t-pham <59778724+vincent-t-pham@users.noreply.github.com>
2022-09-06 02:56:00 -0700

    Update tokenizer.cpp
    Added state diagram for INTEGER and REAL

--- Assignment_2/src/tokenizer.cpp
+++ Assignment_2/src/tokenizer.cpp
@@ -38,7 +38,53 @@ lex::token lex::tokenizer::nextToken() {
         return lex::token{tokentype[tokentypes::LT], lb.getlexeme()};
       }
       break;
+
+    //Vincent 
+    case 12:                      //input that switches to case 12 that just
+      c = lb.nextChar();          //reads next char
+      if (std::isdigit(c)) {      
+        state = 13;               //starts loop to read in expected digits (state 13)
+      }
+      else {
+        panic("should I panic?")  //or should it just move onto the next state if I didn't find a digit
+      }
+    case 13:
+      c = lb.nextChar();
+      if (std::isdigit(c)) {      //keep reading digits until
+        state = 13;               
+      }
+      else if (c == '.'){         //you hit a decimal
+        state = 14;               //then move on to decimal digit loop of a Real Number (14)
+      }
+      else if (c == 'other') {    //otherwise, if you never hit a decimal
+        state = 17;               //state 17 will return an integer
+      }
+    case 14:
+      c = lb.nextChar();          //reads next char for decimal digits
+      if (std::isdigit(c)) {      
+        state = 15;               //starts loop to read in expected digits (state 15)
+      }
+      else {
+        panic("should I panic?")  //or should it just move onto the next state if I didn't find a digit
     }
+    case 15: 
+      c = lb.nextChar();
+      if (std::isdigit(c)) {      //keep reading digits until
+        state = 15;               
+      }
+      else if (c == 'other') {    //you hit the end where
+        state = 16;               //state 16 will return an integer
+      }
+    //case 16: will return an integer
+    //else {
+    //  lb.retract();
+    //  return lex::token{tokentype[tokentypes::INTEGER], lb.getlexeme()};
+    //case 17: will return a real number
+    //else {
+    //  lb.retract();
+    //  return lex::token{tokentype[tokentypes::REAL], lb.getlexeme()};
+
+
   }
 }
 
