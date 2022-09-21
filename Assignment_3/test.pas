57635fbd6c927675ee99d566bea22cb928d34cd7
Andrew <32277829+AndrewDEmerson@users.noreply.github.com>
2022-09-20 17:50:35 -0700

    Shorter Output tree.

--- Assignment_3/test.pas
+++ Assignment_3/test.pas
@@ -4,5 +4,25 @@ begin
  x:=0;
  Y:=5;
  SuperVariableName:=-1;
-
+(*Test Repeat Statement*)
+ repeat
+   x:=1;
+   y:=2;
+ until
+   y = 2;
+(*Test Case Statement*)
+ case grade of
+     1 : Apple:=100;
+     2: Bannana:=200;
+     3,4: Cherry:= 300
+  end;     
+(*Test While Statement*)
+while x < 10 do
+ begin
+   if x*2 < 10+3 then
+     x:= x+1
+   else
+     x:=x+2
+   y:=y*2;
+ end;
 end.
