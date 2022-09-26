46344da96b798ec19006154b88a1746dc568e573
Andrew <32277829+AndrewDEmerson@users.noreply.github.com>
2022-09-25 21:53:38 -0700

    Antlr makes my soul decay.

---
+++ Assignment_4/antlr_gen/src/ExprVisitor.h
@@ -0,0 +1,30 @@
+
+// Generated from src/Expr.g4 by ANTLR 4.11.1
+
+#pragma once
+
+
+#include "antlr4-runtime.h"
+#include "ExprParser.h"
+
+
+
+/**
+ * This class defines an abstract visitor for a parse tree
+ * produced by ExprParser.
+ */
+class  ExprVisitor : public antlr4::tree::AbstractParseTreeVisitor {
+public:
+
+  /**
+   * Visit parse trees produced by ExprParser.
+   */
+    virtual std::any visitProgram(ExprParser::ProgramContext *context) = 0;
+
+    virtual std::any visitStatement(ExprParser::StatementContext *context) = 0;
+
+    virtual std::any visitExpr(ExprParser::ExprContext *context) = 0;
+
+
+};
+
