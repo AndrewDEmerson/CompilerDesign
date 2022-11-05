91264ef745327694c5ad4744c41b9f1b32a6d153
Andrew <32277829+AndrewDEmerson@users.noreply.github.com>
2022-11-04 22:36:00 -0700

    Assignment 6

---
+++ Assignment_6/input.pas
@@ -0,0 +1,16 @@
+Program HelloWorld;
+var 
+  x, y, i: integer;
+  SuperVariableName:real := 2.0;
+begin
+  x:=0;
+  Y:=5;
+  SuperVariableName:=-1;
+  repeat
+    x:=1;
+    y:=2;
+  until
+    y = 2;
+  for i := 0 to 10 do
+    x:= x*10;
+end.
